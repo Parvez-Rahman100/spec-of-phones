e08c9db3c96ec57ed8669d5f8d5ee1b1ef2bfdd0
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-03-01 21:46:38 +0600

    optional chaining added

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 const search = () =>{
+    // searchInput.value = " ";
+    phoneCard.textContent = '';
+    phoneDetailCard.textContent = '';
     const searchInput = document.getElementById('search-input');
     const searchText = searchInput.value;
     // console.log(searchText);
-    searchInput.value = " ";
+    // searchInput.value = " ";
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
     .then(res => res.json())
     .then(data => displaySearchPhones(data.data.slice(0,20)));
+    
 }
 const displaySearchPhones = (phones) =>{
-    phoneCard.textContent = '';
+    
+    
     phones.forEach(phone => {
         const phoneCard = document.getElementById('phoneCard');
         // console.log(phone);
@@ -17,7 +22,7 @@ const displaySearchPhones = (phones) =>{
         div.classList.add('col');
         div.innerHTML = `
         <div class="card">
-                 <img src="${phone.image}" class="card-img-top w-50"  alt="Phones">
+                 <img src="${phone.image}" class="card-img-top w-50 mx-auto"  alt="Phones">
                  <div class="card-body">
                   <h5 class="card-title">${phone.phone_name}</h5>
                   <p class="card-text">${phone.brand}</p>
@@ -43,24 +48,25 @@ const displayDetails = (details) =>{
     phoneDetailCard.textContent = '';
     const div = document.createElement('div');
     div.innerHTML = `
-    <div class="card" style="width: 18rem;">
+    <div class="card mx-auto" style="width: 18rem;">
     <div class="card-body">
-    <img src="${details.image}" class="card-img-top w-50 " alt="Phone">
+    <img src="${details.image}" class="card-img-top w-50 mx-auto" alt="Phone">
     <h5 class="card-title">${details.name}</h5>
     <h5>${details.brand}</h5>
     <p>${details.releaseDate ? details.releaseDate : 'Release Date Not Found'}</p>
+    <p>Sensors: ${details.mainFeatures.sensors}</p>
     <h6>Main Features</h6>
     <p>${details.mainFeatures.chipSet}</p>
     <p>${details.mainFeatures.storage}</p>
     <p>${details.mainFeatures.displaySize}</p>
     <p>${details.mainFeatures.memory}</p>
     <h6>Others</h6>
-    <p>Blutooth :${details.others.Bluetooth}</p>
-    <p>GPS :${details.others.GPS}</p>
-    <p>NFC :${details.others.NFC}</p>
-    <p>Radio :${details.others.Radio}</p>
-    <p>USB :${details.others.USB}</p>
-    <p>WLAN :${details.others.WLAN}</p>
+    <p>Blutooth :${details.others?.Bluetooth}</p>
+    <p>GPS :${details.others?.GPS}</p>
+    <p>NFC :${details.others?.NFC}</p>
+    <p>Radio :${details.others?.Radio}</p>
+    <p>USB :${details.others?.USB}</p>
+    <p>WLAN :${details.others?.WLAN}</p>
   </div>
 </div>
     `;
